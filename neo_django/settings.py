f9971d2681062443d2dd55f49cf942d9601b5e42
Bekzod Imomov <magiccodeuz@gmail.com>
2026-02-02 22:30:13 +0500

    127.0.0.1

--- neo_django/settings.py
+++ neo_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['neooptika.uz','www.neooptika.uz']
+ALLOWED_HOSTS = ['neooptika.uz','www.neooptika.uz', '127.0.0.1']
 
 
 # Application definition
